df4fd859248084cd1fdb0cf9e8267640463900c7
Radouane Chihaby <chihaby@gmail.com>
2025-10-03 10:26:25 -0700

    updates keys

--- config/admin.ts
+++ config/admin.ts
@@ -2,7 +2,7 @@ export default ({ env }: { env: (key: string, defaultValue?: any) => any }) => {
 
   return {
     auth: {
-      secret: env('ADMIN_JWT_SECRET'),
+      secret: env('d11441745ae5243562eb27c2ba4426ef74d9a57a206ef7550de189bae961b3c1'),
     },
     apiToken: {
       salt: env('API_TOKEN_SALT'),
